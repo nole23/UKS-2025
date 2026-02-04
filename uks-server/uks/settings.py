bad0d4f0cab71076fd6360c134f09c05b2219770
markobanja <markobanja@gmail.com>
2026-02-04 23:48:11 +0100

    NGINX reverse proxy not forwarding API requests #45

--- uks-server/uks/settings.py
+++ uks-server/uks/settings.py
@@ -22,7 +22,7 @@ DEBUG = config('DEBUG', default=True, cast=bool)
 
 ALLOWED_HOSTS = config(
     'ALLOWED_HOSTS',
-    default='127.0.0.1,localhost',
+    default='localhost,127.0.0.1,nginx,nginx-proxy,web',
     cast=lambda v: [s.strip() for s in v.split(',')]
 )
 
