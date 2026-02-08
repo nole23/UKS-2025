f584cb0baa81cbbbc86430f1d6ea0ddce302a5e3
nole23 <nole0223@gmail.com>
2026-02-08 02:11:22 +0100

    Create Settings for repository #15

--- uks-server/user/models.py
+++ uks-server/user/models.py
@@ -31,8 +31,6 @@ class UserProfile(models.Model):
         related_name="profile"
     )
 
-    first_name = models.CharField(max_length=100, blank=True)
-    last_name = models.CharField(max_length=100, blank=True)
     bio = models.TextField(blank=True)
     avatar = models.ImageField(
         upload_to="avatars/",
@@ -48,6 +46,8 @@ class UserProfile(models.Model):
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    default_repository = models.BooleanField(default=True)
 
     def __str__(self):
         return f"Profile({self.user.username})"
